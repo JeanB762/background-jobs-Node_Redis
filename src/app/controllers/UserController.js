4ec92fff18a306b6a645fce0a89abd5d25f3e44d
Jean Borges <jeanborges946@gmail.com>
2020-05-28 15:26:32 -0300

    feature: mail config

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,3 +1,5 @@
+import Mail from '../lib/Mail';
+
 export default {
   async store(req, res) {
     const { name, email, password } = req.body;
@@ -7,6 +9,13 @@ export default {
       password,
 
     };
+
+    await Mail.sendMail({
+      from: 'Queue Test <queue@teste.com>',
+      to: `${name} <${email}>`,
+      subject: 'Cadastro de usuarios',
+      html: `olá, ${name}, bem vindo ao sistema de filas :D`,
+    });
     return res.json(user);
   },
 };
